e18a01a88407fb54f63a611da77211dbc47b78e9
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-01-23 22:24:22 +0600

    sign up

--- signup.php
+++ signup.php
@@ -1,8 +1,33 @@
 <?php require "header.php"?>
 
 <main class="container ">
-    <p>You are logged out</p>
-    <p>You are logged out</p>
+    <h1>Signup</h1>
+    <div class="row">
+        <form action="includes/signup.inc.php" method="post" class="col s12">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input type="text" name="uid" placeholder="Username">
+                </div>
+                <div class="input-field col s6">
+                    <input type="email" name="mail" placeholder="E-mail">
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input type="password" name="pwd" placeholder="Password">
+                </div>
+                <div class="input-field col s6">
+                    <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+                </div>
+            </div>
+
+
+
+
+            <!--        HERE I MADE THE MISTAKE IN PREVIOUS PROJECT. SUBMIT INPUT FIELD SHOULD HAVE A NAME-->
+            <input type="submit" value="Signup" name="signup-submit" class="btn #004d40 teal darken-4">
+        </form>
+    </div>
 </main>
 
 <?php require "footer.php" ?>
