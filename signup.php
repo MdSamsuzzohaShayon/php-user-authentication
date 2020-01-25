3c9e86089c3008ab3939022d5d881488590b24af
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-01-25 14:39:08 +0600

    show error or successfull message

--- signup.php
+++ signup.php
@@ -2,11 +2,30 @@
 
 <main class="container ">
     <h1>Signup</h1>
+    <?php
+        if(isset($_GET['error'])){
+            if ($_GET['error']== "emptyfields"){
+                echo '<p class="card-panel #d32f2f red darken-2">Fill all the forms</p>';
+            }else if($_GET['error']== "invalidmailuid"){
+                echo '<p class="card-panel #d32f2f red darken-2">Invalid user name and e-mail</p>';
+            }else if($_GET['error']== "invalidmail"){
+                echo '<p class="card-panel #d32f2f red darken-2">Invalid E-mail</p>';
+            }else if($_GET['error']== "invaliduidl"){
+                echo '<p class="card-panel #d32f2f red darken-2">Invalid Username</p>';
+            }else if($_GET['error']== "passwordcheck"){
+                echo '<p class="card-panel #d32f2f red darken-2">Your password did not match</p>';
+            }else if($_GET['error']== "usertaken"){
+                echo '<p class="card-panel #d32f2f red darken-2">User is already taken</p>';
+            }
+        }else if($_GET['signup'] == 'success'){
+            echo '<p class="card-panel #388e3c green darken-2">Sign up successful </p>';
+        }
+    ?>
     <div class="row">
         <form action="includes/signup.inc.php" method="post" class="col s12">
             <div class="row">
                 <div class="input-field col s6">
-                    <input type="text" name="uid" placeholder="Username" c>
+                    <input type="text" name="uid" placeholder="Username" >
                 </div>
                 <div class="input-field col s6">
                     <input type="email" name="mail" placeholder="E-mail">
